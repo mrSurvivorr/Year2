21cb40a1578b14cd52c0a310468ac12a4fab54b3
Edward <31244367+mrSurvivorr@users.noreply.github.com>
2018-08-17 13:42:14 +0300

    Add files via upload

---
+++ Lab_07/Lab_7/Script.js
@@ -0,0 +1,124 @@
+﻿function sin()
+{
+	var number, res;
+	
+	number = document.getElementById('num').value;
+	number = parseFloat(number);
+	
+	res = Math.sin(number);
+	
+	document.getElementById('result').innerHTML = res;
+}
+
+function cos()
+{
+	var number, res;
+	
+	number = document.getElementById('num').value;
+	number = parseFloat(number);
+	
+	res = Math.cos(number);
+	
+	document.getElementById('result').innerHTML = res;
+}
+
+function tg()
+{
+	var number, res;
+	
+	number = document.getElementById('num').value;
+	number = parseFloat(number);
+	
+	res = Math.tan(number);
+	
+	document.getElementById('result').innerHTML = res;
+}
+
+function ctg()
+{
+	var number, res;
+	
+	number = document.getElementById('num').value;
+	number = parseFloat(number);
+	
+	number = Math.tan(number);
+	
+	res = Math.pow(number, -1);
+	
+	document.getElementById('result').innerHTML = res;
+}
+
+function pow()
+{
+	var number, exp, res;
+	
+	number = document.getElementById('num').value;
+	number = parseFloat(number);
+	
+	exp = document.getElementById('n').value;
+	exp = parseFloat(exp);
+	
+	res = Math.pow(number, exp);
+	
+	document.getElementById('result').innerHTML = res;
+}
+
+function rt()
+{
+	var number, exp, res;
+	
+	number = document.getElementById('num').value;
+	number = parseFloat(number);
+	
+	exp = document.getElementById('n').value;
+	exp = parseFloat(exp);
+	
+	res = Math.pow(number, (1/exp));
+	
+	document.getElementById('result').innerHTML = res;
+}
+
+function rgb()
+{
+	var color, r, g, b;
+	
+	var table = document.getElementById("cols").rows;
+	var cell;
+	for(i = 0; i < 8; i++)
+	{    for(j = 0; j < 8; j++)
+     	{
+     	    cell = table[i].cells;
+
+			color = Math.round(255.0*Math.random());
+			r = color.toString(16);
+	
+			color = Math.round(255.0*Math.random());
+			g = color.toString(16);
+	
+			color = Math.round(255.0*Math.random());
+			b = color.toString(16);
+	
+			color = r + g + b;
+			
+			cell[j].style.background = "#" + color;         	
+         	
+         	cell[j].innerHTML = color;
+     	}
+	}     	
+}
+
+function pay()
+{
+	var sum, sub, yield;
+	
+	sum = document.getElementById('pay').value;
+	sum = parseFloat(sum);
+	
+	sub = sum*0.18;
+	
+	yield = sum - sub;
+
+	newwin = window.open();
+	
+	newwin.document.write('<table style="position: absolute; left: 25%; width: 50%;padding: 1px;margin: 2px;color: black;text-align: center;border: 1px solid black;"><tr style="border: 1px solid black;"><th style="border: 1px solid black;">Payment</th><th style="border: 1px solid black;">ACT</th><th style="border: 1px solid black;">Yield</th></tr><tr style="border: 1px solid black;"><td style="border: 1px solid black;">' + sum + '</td><td style="border: 1px solid black;">' + sub + '</td><td style="border: 1px solid black;">' + yield + '</td></table>');
+}
---
+++ Lab_07/Lab_7/_vti_cnf/Script.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Edward99-ПК\\Edward99
+vti_modifiedby:SR|Edward99-ПК\\Edward99
+vti_timelastmodified:TR|26 Apr 2018 12:21:04 -0000
+vti_timecreated:TR|24 Apr 2018 15:14:52 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|colors.html default.html profits.html
+vti_nexttolasttimemodified:TW|24 Apr 2018 18:31:55 -0000
+vti_cacheddtm:TX|26 Apr 2018 12:21:04 -0000
+vti_filesize:IR|2813
